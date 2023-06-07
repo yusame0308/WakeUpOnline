62eee8490d279af76dd2937a68efa4366c2e93bd
yusame <yu.starts.yu@gmail.com>
2023-06-08 01:54:31 +0900

    first commit

---
+++ WakeUpOnline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WakeUpOnline
+//
+//  Created by 小原宙 on 2023/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
