8bc1f99226c190a45e869a4f700d62d1b9096859
yusame <yu.starts.yu@gmail.com>
2023-06-08 15:50:28 +0900

    ColorAssetを追加

---
+++ WakeUpOnline/Extension/UIColor-Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor-Extension.swift
+//  WakeUpOnline
+//
+//  Created by 小原宙 on 2023/06/08.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    static let darkBrown = UIColor(named: "DarkBrown")!
+    static let blackBrown = UIColor(named: "BlackBrown")!
+    static let lightBrown = UIColor(named: "LightBrown")!
+    static let grayBrown = UIColor(named: "GrayBrown")!
+    static let accentBrown = UIColor(named: "AccentBrown")!
+}
