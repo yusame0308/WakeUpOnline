bd8bf35d7f9df7daa442f78052993ae17a7d946a
yusame <yu.starts.yu@gmail.com>
2023-06-11 20:23:33 +0900

    InfoCardViewを追加

---
+++ WakeUpOnline/View/Home/InfoCardView.swift
@@ -0,0 +1,40 @@
+//
+//  InfoCardView.swift
+//  WakeUpOnline
+//
+//  Created by 小原宙 on 2023/06/08.
+//
+
+import UIKit
+import SnapKit
+
+final class InfoCardView: UIView {
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 100
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "Ski-Parking")
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        self.addSubview(iconImageView)
+        
+        iconImageView.snp.makeConstraints { make in
+            make.width.equalTo(20)
+            make.height.equalTo(20)
+            make.center.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
