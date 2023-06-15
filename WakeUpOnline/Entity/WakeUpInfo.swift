b2d82eccc975aa6f302e3e9f44d7967a2dbc259d
yusame <yu.starts.yu@gmail.com>
2023-06-16 00:51:10 +0900

    起床時間の文字列を取得する処理を追加

--- WakeUpOnline/Entity/WakeUpInfo.swift
+++ WakeUpOnline/Entity/WakeUpInfo.swift
@@ -14,14 +14,20 @@ struct WakeUpInfo {
     let iconUrl: String
     let totalDates: Int
     let consecutiveDates: Int
+
+    var timeText: String {
+        var timeString = String(time)
+        timeString.insert(":", at: timeString.index(timeString.endIndex, offsetBy: -2))
+        return timeString
+    }
 }
 
 let wakeUpInfos = [
-    WakeUpInfo(userName: "太郎", message: "がんばります", time: 0800, iconUrl: "", totalDates: 10, consecutiveDates: 3),
-    WakeUpInfo(userName: "山田", message: "早起きがんばります", time: 0830, iconUrl: "", totalDates: 4, consecutiveDates: 2),
-    WakeUpInfo(userName: "たろう", message: "がんばります", time: 0900, iconUrl: "", totalDates: 8, consecutiveDates: 5),
-    WakeUpInfo(userName: "太郎", message: "早起きがんばります", time: 0915, iconUrl: "", totalDates: 2, consecutiveDates: 2),
-    WakeUpInfo(userName: "山田", message: "がんばります", time: 0920, iconUrl: "", totalDates: 1, consecutiveDates: 0),
-    WakeUpInfo(userName: "タロウ", message: "がんばります", time: 0945, iconUrl: "", totalDates: 9, consecutiveDates: 3),
+    WakeUpInfo(userName: "太郎", message: "がんばります", time: 800, iconUrl: "", totalDates: 10, consecutiveDates: 3),
+    WakeUpInfo(userName: "山田", message: "早起きがんばります", time: 830, iconUrl: "", totalDates: 4, consecutiveDates: 2),
+    WakeUpInfo(userName: "たろう", message: "がんばります", time: 900, iconUrl: "", totalDates: 8, consecutiveDates: 5),
+    WakeUpInfo(userName: "太郎", message: "早起きがんばります", time: 915, iconUrl: "", totalDates: 2, consecutiveDates: 2),
+    WakeUpInfo(userName: "山田", message: "がんばります", time: 920, iconUrl: "", totalDates: 1, consecutiveDates: 0),
+    WakeUpInfo(userName: "タロウ", message: "がんばります", time: 945, iconUrl: "", totalDates: 9, consecutiveDates: 3),
     WakeUpInfo(userName: "やまだ", message: "がんばります", time: 1000, iconUrl: "", totalDates: 5, consecutiveDates: 5)
 ]
