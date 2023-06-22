4b7d96205599d5fcddaa879b8fc1b923c3a64a1f
yusame <yu.starts.yu@gmail.com>
2023-06-22 22:15:42 +0900

    DailyRecordViewを追加

---
+++ WakeUpOnline/View/Common/DailyLogCollectionView.swift
@@ -0,0 +1,61 @@
+//
+//  DailyLogCollectionView.swift
+//  WakeUpOnline
+//
+//  Created by 小原宙 on 2023/06/20.
+//
+
+import UIKit
+
+final class DailyLogCollectionView: UICollectionView, UICollectionViewDataSource {
+
+    private let cellID = "dailyLogCellID"
+
+    // layout
+    private let horizontalItemCount = 7
+    private let verticalItemCount = 5
+    private let itemSpacing: CGFloat = 3
+
+    init(width: CGFloat) {
+        // FlowLayout
+        let flowLayout = UICollectionViewFlowLayout()
+        let availableWidth = Int(width) - Int(itemSpacing) * (horizontalItemCount - 1)
+        let itemWidth = availableWidth / 7
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth)
+        flowLayout.minimumInteritemSpacing = itemSpacing
+        flowLayout.minimumLineSpacing = itemSpacing
+
+        super.init(frame: .zero, collectionViewLayout: flowLayout)
+
+        self.dataSource = self
+        self.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+
+        // 自身のサイズを指定
+        self.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: width, height: selfHeight(of: width)))
+        }
+    }
+
+    // 自身の幅から高さを計算
+    private func selfHeight(of width: CGFloat) -> CGFloat {
+        let itemWidth = (Int(width) - Int(itemSpacing) * (horizontalItemCount - 1)) / horizontalItemCount
+        let selfHeight = itemWidth * verticalItemCount + Int(itemSpacing) * (verticalItemCount - 1)
+        return CGFloat(selfHeight)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return horizontalItemCount * verticalItemCount
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = self.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        cell.backgroundColor = .accentBrown
+        cell.layer.cornerRadius = 4
+        return cell
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
---
+++ WakeUpOnline/View/Common/DailyRecordView.swift
@@ -0,0 +1,86 @@
+//
+//  DailyRecordView.swift
+//  WakeUpOnline
+//
+//  Created by 小原宙 on 2023/06/22.
+//
+
+import UIKit
+
+final class DailyRecordView: UIView {
+
+    // アイテムの幅
+    private let itemWidth: CGFloat
+
+    // デイリーログ
+    private let dailyLogCollectionView: DailyLogCollectionView
+
+    // 達成記録ラベル
+    private let recordLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        label.textColor = .accentBrown
+        label.numberOfLines = 2
+        return label
+    }()
+
+    // 達成記録
+    private lazy var recordLabelItem: UIView = {
+        let view = UIView()
+        view.addSubview(recordLabel)
+        recordLabel.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
+        view.snp.makeConstraints { make in
+            make.width.equalTo(itemWidth)
+        }
+        return view
+    }()
+
+    init(viewWidth: CGFloat, recordText: String) {
+        itemWidth = viewWidth / 2 - 50
+        dailyLogCollectionView = DailyLogCollectionView(width: itemWidth)
+        recordLabel.text = recordText
+
+        super.init(frame: .zero)
+
+        setupLayout()
+
+        self.backgroundColor = .white
+        self.addShadow()
+    }
+
+    private func setupLayout() {
+        // 全体のStackView
+        let baseStackView = UIStackView(arrangedSubviews: [dailyLogCollectionView, recordLabelItem])
+        baseStackView.axis = .horizontal
+        baseStackView.distribution = .equalSpacing
+
+        self.addSubview(baseStackView)
+
+        baseStackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(10)
+            make.left.equalToSuperview().offset(15)
+            make.bottom.equalToSuperview().offset(-10)
+            make.right.equalToSuperview().offset(-15)
+        }
+
+        // 区切り線
+        let divider = UIView()
+        divider.backgroundColor = .blackBrown.withAlphaComponent(0.2)
+
+        self.addSubview(divider)
+
+        divider.snp.makeConstraints { make in
+            make.width.equalTo(1)
+            make.height.equalTo(baseStackView)
+            make.center.equalToSuperview()
+        }
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
