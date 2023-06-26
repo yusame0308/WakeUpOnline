2d414e6eb1abdcd84a62f21e5b63f7ede0c55b19
yusame <yu.starts.yu@gmail.com>
2023-06-26 17:18:33 +0900

    コメントを追加

--- WakeUpOnline/Extension/String-Extension.swift
+++ WakeUpOnline/Extension/String-Extension.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension String {
+    // 行間を指定
     func attributedStringWithLineHeightMultiple(by heightMultiple: CGFloat) -> NSAttributedString {
         let paragraph = NSMutableParagraphStyle()
         paragraph.lineHeightMultiple = heightMultiple
@@ -15,6 +16,7 @@ extension String {
         return NSAttributedString(string: self, attributes: [.paragraphStyle: paragraph])
     }
 
+    // 文字間隔を指定
     func attributedStringWithKern(_ kern: CGFloat) -> NSAttributedString {
         return NSAttributedString(string: self, attributes: [.kern: kern])
     }
