25bd2d62c1cd77a4af308fafb464028100a85ecf
yusame <yu.starts.yu@gmail.com>
2023-06-26 17:58:40 +0900

    MypageEdit画面を追加

---
+++ WakeUpOnline/View/MypageEdit/MypageEditViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MypageEditViewController.swift
+//  WakeUpOnline
+//
+//  Created by 小原宙 on 2023/06/26.
+//
+
+import UIKit
+
+final class MypageEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+}
