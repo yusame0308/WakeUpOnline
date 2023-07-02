fb2902006682d6e8407f8aa5699877ae6e937082
yusame <yu.starts.yu@gmail.com>
2023-07-03 01:38:28 +0900

    TimeListにstringValues()を追加

--- WakeUpOnline/Entity/TimeList.swift
+++ WakeUpOnline/Entity/TimeList.swift
@@ -16,18 +16,25 @@ struct TimeList {
     var friday: Int
     var saturday: Int
 
-    // index(1〜7)から値を取得
-    func value(of index: Int) -> Int {
+    // index(1〜7)から画面表示用のデータを取得
+    func stringValues(of index: Int) -> (weekDay: String, time: String) {
         switch index {
-        case 1: return sunday
-        case 2: return monday
-        case 3: return tuesday
-        case 4: return wednesday
-        case 5: return thursday
-        case 6: return friday
-        case 7: return saturday
-        default: return monday
+        case 1: return ("日", timeText(of: sunday))
+        case 2: return ("月", timeText(of: monday))
+        case 3: return ("火", timeText(of: tuesday))
+        case 4: return ("水", timeText(of: wednesday))
+        case 5: return ("木", timeText(of: thursday))
+        case 6: return ("金", timeText(of: friday))
+        case 7: return ("土", timeText(of: saturday))
+        default: return ("月", timeText(of: monday))
         }
+    }
+
+    // 画面表示のための起床時刻の文字列
+    private func timeText(of time: Int) -> String {
+        var timeString = String(time)
+        timeString.insert(":", at: timeString.index(timeString.endIndex, offsetBy: -2))
+        return timeString
     }
 }
 
