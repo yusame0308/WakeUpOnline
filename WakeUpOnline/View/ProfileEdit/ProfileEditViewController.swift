77d197d1929b6da6fbede0ed71a94c495cbcb146
yusame <yu.starts.yu@gmail.com>
2023-07-06 05:24:56 +0900

    アイコン編集マークを追加

--- WakeUpOnline/View/ProfileEdit/ProfileEditViewController.swift
+++ WakeUpOnline/View/ProfileEdit/ProfileEditViewController.swift
@@ -11,14 +11,26 @@ final class ProfileEditViewController: UIViewController {
 
     // アイコンの大きさ
     private static let iconWidth: CGFloat = 100
+    private static let editImageWidth: CGFloat = 25
 
-    // アイコン
-    private let iconImageView: UIImageView = {
-        let imageView = UIImageView()
-        // 画像を正方形にリサイズ
-        imageView.image = R.image.testImage1()?.cropResizedSquare(iconWidth)
-        imageView.layer.cornerRadius = iconWidth * 0.5
-        imageView.clipsToBounds = true
+    // アイコンボタン
+    private let iconButton: UIButton = {
+        // 画像をセット
+        var config = UIButton.Configuration.plain()
+        config.image = R.image.testImage1()?.cropResizedSquare(iconWidth)
+        config.contentInsets = .zero
+        let button = UIButton(configuration: config)
+        button.layer.cornerRadius = iconWidth * 0.5
+        button.clipsToBounds = true
+        return button
+    }()
+
+    // 編集マーク
+    private let editImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "pencil.circle.fill")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold)))
+        imageView.tintColor = .blackBrown
+        imageView.backgroundColor = .white
+        imageView.layer.cornerRadius = editImageWidth * 0.5
         return imageView
     }()
 
@@ -49,7 +61,7 @@ final class ProfileEditViewController: UIViewController {
 
     private func setupLayout() {
         // プロフィールのStackView
-        let baseStackView = UIStackView(arrangedSubviews: [iconImageView, userNameTextField, messageTextField])
+        let baseStackView = UIStackView(arrangedSubviews: [iconButton, userNameTextField, messageTextField])
         baseStackView.axis = .vertical
         baseStackView.alignment = .center
         baseStackView.spacing = 20
@@ -72,6 +84,14 @@ final class ProfileEditViewController: UIViewController {
         messageTextField.snp.makeConstraints { make in
             make.horizontalEdges.equalTo(baseStackView)
             make.height.equalTo(textFieldHeight)
+        }
+
+        // アイコンの編集マーク
+        view.addSubview(editImageView)
+        editImageView.snp.makeConstraints { make in
+            make.top.equalTo(iconButton).offset(5)
+            make.right.equalTo(iconButton).offset(-5)
+            make.size.equalTo(CGSize(width: ProfileEditViewController.editImageWidth, height: ProfileEditViewController.editImageWidth))
         }
     }
 
