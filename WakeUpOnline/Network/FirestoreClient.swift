07bf275a2ff5249137d6a0246dfc84dbd75bb0ff
yusame <yu.starts.yu@gmail.com>
2023-07-21 23:59:59 +0900

    FirestoreClientを追加

---
+++ WakeUpOnline/Network/FirestoreClient.swift
@@ -0,0 +1,14 @@
+//
+//  FirestoreClient.swift
+//  WakeUpOnline
+//
+//  Created by 小原宙 on 2023/07/20.
+//
+
+import Foundation
+
+protocol FirestoreClientable {
+}
+
+final class FirestoreClient: FirestoreClientable {
+}
