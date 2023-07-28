a0bb88e9f7df2ec5558b0875623b4c1b0c3dc9cd
yusame <yu.starts.yu@gmail.com>
2023-07-28 22:04:04 +0900

    UserにiconUrlを追加

--- WakeUpOnline/Entity/User.swift
+++ WakeUpOnline/Entity/User.swift
@@ -11,6 +11,7 @@ struct User: Codable {
     let id: String // ID
     let name: String // 名前
     let message: String // メッセージ
+    let iconUrl: String // アイコンURL
     let wakeUpTimeList: WakeUpTimeList // 起床時刻リスト
     let wakeUpLog: WakeUpLog // 起床記録
 
@@ -19,30 +20,32 @@ struct User: Codable {
         case id
         case name
         case message
+        case iconUrl = "icon_url"
         case wakeUpTimeList = "wake_up_time_list"
         case wakeUpLog = "wake_up_log"
     }
 
-    init(id: String, name: String, message: String, wakeUpTimeList: WakeUpTimeList, wakeUpLog: WakeUpLog) {
+    init(id: String, name: String, message: String, iconUrl: String, wakeUpTimeList: WakeUpTimeList, wakeUpLog: WakeUpLog) {
         self.id = id
         self.name = name
         self.message = message
+        self.iconUrl = iconUrl
         self.wakeUpTimeList = wakeUpTimeList
         self.wakeUpLog = wakeUpLog
     }
 
     // IDのみ指定するinit
     init(id: String) {
-        self.init(id: id, name: "名無し", message: "よろしくお願いします", wakeUpTimeList: WakeUpTimeList(), wakeUpLog: WakeUpLog())
+        self.init(id: id, name: "名無し", message: "よろしくお願いします", iconUrl: "", wakeUpTimeList: WakeUpTimeList(), wakeUpLog: WakeUpLog())
     }
 }
 
 let users = [
-    User(id: "test-id-1", name: "太郎", message: "がんばります", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
-    User(id: "test-id-2", name: "山田", message: "よろしくお願いします。", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
-    User(id: "test-id-3", name: "テストユーザー", message: "がんばります", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
-    User(id: "test-id-4", name: "田中", message: "よろしくお願いします。よろしくお願いします。", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
-    User(id: "test-id-5", name: "テストテストテストテストテストテストテストテストテスト", message: "がんばります", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
-    User(id: "test-id-6", name: "タロウ", message: "よろしくお願いします。", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
-    User(id: "test-id-7", name: "やまだ", message: "がんばります", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog)
+    User(id: "test-id-1", name: "太郎", message: "がんばります", iconUrl: "", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
+    User(id: "test-id-2", name: "山田", message: "よろしくお願いします。", iconUrl: "", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
+    User(id: "test-id-3", name: "テストユーザー", message: "がんばります", iconUrl: "", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
+    User(id: "test-id-4", name: "田中", message: "よろしくお願いします。よろしくお願いします。", iconUrl: "", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
+    User(id: "test-id-5", name: "テストテストテストテストテストテストテストテストテスト", message: "がんばります", iconUrl: "", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
+    User(id: "test-id-6", name: "タロウ", message: "よろしくお願いします。", iconUrl: "", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog),
+    User(id: "test-id-7", name: "やまだ", message: "がんばります", iconUrl: "", wakeUpTimeList: testWakeUpTimeList, wakeUpLog: testWakeUpLog)
 ]
