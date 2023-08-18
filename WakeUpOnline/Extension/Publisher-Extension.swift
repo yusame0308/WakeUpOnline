f3713ed84373c441b01389606b214d5fd22a6367
yusame <yu.starts.yu@gmail.com>
2023-08-18 23:59:04 +0900

    Publisher-Extensionを追加

---
+++ WakeUpOnline/Extension/Publisher-Extension.swift
@@ -0,0 +1,26 @@
+//
+//  Publisher-Extension.swift
+//  WakeUpOnline
+//
+//  Created by 小原宙 on 2023/08/18.
+//
+
+import Combine
+
+extension Publisher {
+    public func tryAwaitMap<T>(_ transform: @escaping (Self.Output) async throws -> T) -> Publishers.FlatMap<Future<T, Error>, Self> {
+        flatMap { value in
+            Future { promise in
+                Task {
+                    do {
+                        let result = try await transform(value)
+                        promise(.success(result))
+                    }
+                    catch {
+                        promise(.failure(error))
+                    }
+                }
+            }
+        }
+    }
+}
